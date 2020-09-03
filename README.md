a0a24dbcb33186c749b6eeab65af3c1b8317afc7
WenYang-Lai <x3639026@gmail.com>
2020-09-03 09:21:04 +0000

    {Update}readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # In-Vivo Fuzzing Test Framework
+
+
+
+### Compilation
+
+- Complie runtime library
+- Compile LLVM BSA Mode
+- Use following flags
+
+```
+CC=afl-IV-clang-fast CFLAGS="-mllvm -config=/root/nginx-1.4.0/entry.conf -mllvm -level=2 -pthread
+```
+
+### Launch IA server
+```
+python3 IA_server.py
+```
+
+### Send Fuzzing Request
+```
+./req type pid tid bbid
+```
+
---
+++ req.c
@@ -0,0 +1,61 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stddef.h>
+#include <stdio.h>
+
+
+int main(int argc, char** argv){
+    struct sockaddr_un addr;
+    int addr_len;
+    int sockfd;
+    int is_report = 0, pid, tid, bid, val, type = atoi(argv[1]);
+
+
+    if (type == 1){
+        is_report = 0;
+        pid = atoi(argv[2]);
+        tid = atoi(argv[3]);
+        bid = atoi(argv[4]);
+        
+    }else if (type == 2){
+        is_report = 1;
+        pid = atoi(argv[2]);
+        bid = atoi(argv[3]);
+        val = atoi(argv[4]);
+    }else{
+        printf("No such req\n");
+        exit(0);
+    }
+
+    sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    
+    memset(&addr, 0, sizeof(struct sockaddr_un)); 
+    addr.sun_family = AF_LOCAL;
+    sprintf(addr.sun_path, "/tmp/BSA_req_%d.sock", pid);
+    addr_len = offsetof(struct sockaddr_un, sun_path) + strlen(addr.sun_path);
+    if(connect(sockfd, (struct sockaddr*)&addr, addr_len) < 0){
+       perror("Can't connect bsa remote file server");
+    }
+
+    if (is_report == 0){
+        int quest[2];
+        quest[0] = 1;
+        quest[1] = tid ^ (bid<<16) ;
+        if (write(sockfd, quest, 8) != 8){
+            perror("write failed");
+        }
+    }else{
+        int report[4];
+        report[0] = 2;
+        report[1] = getpid();
+        report[2] = bid;
+        report[3] = val;
+
+        write(sockfd, report, 16);
+        pause();
+    }
+    
+}
