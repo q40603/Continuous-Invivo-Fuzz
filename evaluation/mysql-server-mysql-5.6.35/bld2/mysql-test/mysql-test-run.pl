4b4d008ea99810fb957fa6dd9a3943032ccf3879
WenYang-Lai <x3639026@gmail.com>
2020-09-08 10:27:34 +0000

    {add}[eval]

---
+++ evaluation/mysql-server-mysql-5.6.35/bld2/mysql-test/mysql-test-run.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+# Call mtr in out-of-source build
+$ENV{MTR_BINDIR} = '/root/eval/mysql-server-mysql-5.6.35/bld2';
+chdir('/root/eval/mysql-server-mysql-5.6.35/mysql-test');
+exit(system($^X, '/root/eval/mysql-server-mysql-5.6.35/mysql-test/mysql-test-run.pl', @ARGV) >> 8);
